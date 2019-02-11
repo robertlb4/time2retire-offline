5ac81827ee3eb193d78fbac4fba439b2e97014a6
Robert Brown <robertlb4@gmail.com>
2019-02-11 11:37:09 -0800

    fix environment pathing and install express

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,23 +5,28 @@ var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
 var env = process.env.IONIC_ENV;
 
-useDefaultConfig.prod.resolve.alias = {
-  '@app/env': path.resolve(environmentPath('prod'))
-};
+// useDefaultConfig.prod.resolve.alias = {
+//   '@app/env': path.resolve(environmentPath('prod'))
+// };
 
-useDefaultConfig.dev.resolve.alias = {
-  '@app/env': path.resolve(environmentPath('dev'))
-};
+// useDefaultConfig.dev.resolve.alias = {
+//   '@app/env': path.resolve(environmentPath('dev'))
+// };
 
-if (env !== 'prod' && env !== 'dev') {
+if (env !== 'prod') {
   // Default to dev config
   useDefaultConfig[env] = useDefaultConfig.dev;
   useDefaultConfig[env].resolve.alias = {
     '@app/env': path.resolve(environmentPath(env))
   };
+} else {
+  useDefaultConfig.prod.resolve.alias = {
+    '@app/env': path.resolve(environmentPath('prod'))
+  };
 }
 
 function environmentPath(env) {
+  // console.log('env:', env);
   var filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
   if (!fs.existsSync(filePath)) {
     console.log(chalk.red('\n' + filePath + ' does not exist!'));
